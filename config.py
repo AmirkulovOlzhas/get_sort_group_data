d3d1bdde486093b05f601e57ebaf1d86c7cc79c7
Olzhas <vip0amirkulov@gmail.com>
2022-10-14 11:43:25 +0600

    works

--- config.py
+++ config.py
@@ -1,39 +1,7 @@
-#list_p = {"alatau": "+774776065", "kasiret": "+7750261982", "nursat": "+77072891276", "pobeda": "+77056935245"}
+contact = "Содержание парков"
+archive = "_3hD9c"
+chat = "n5hs2j7m oq31bsqd gx1rr48f qh5tioqs"
+message_class="i0jNr selectable-text copyable-text"
+photo_class = "jciay5ix tvf2evcx oq44ahr5 lb5m6g5c"
+list_p = {"alatau": "774776065", "kasiret": "7750261982", "nursat": "77072891276", "pobeda": "77056935245"}
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-import time
-
-contact = "Бату"
-text = "Hey, this message was sent using Selenium"
-
-options = webdriver.ChromeOptions()
-options.add_argument(r'--user-data-dir=C:\Users\OFFICE\AppData\Local\Google\Chrome\User Data\Default')
-options.add_argument('--profile-directory=Default')
-driver = webdriver.Chrome(options=options)
-
-driver.get("https://web.whatsapp.com")
-
-print("Scan QR Code, And then Enter")
-input()
-print("Logged In")
-# inp_xpath_search = "//input[@title='Search or start new chat']"
-# input_box_search = WebDriverWait(driver,50).until(lambda driver: driver.find_element(inp_xpath_search))
-# input_box_search.click()
-# time.sleep(2)
-# input_box_search.send_keys(contact)
-time.sleep(2)
-selected_contact = driver.find_element(By.XPATH, '//span[@title="{}"]'.format(contact))
-print("success")
-selected_contact.click()
-inp_xpath = '//div[@class="copyable-text"]'
-input_box = driver.find_element(By.XPATH, inp_xpath)
-time.sleep(2)
-for i in input_box:
-    print(i)
-time.sleep(2)
-driver.quit()
--- main.py
+++ main.py
@@ -1,26 +1,55 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
-import config
-from webdriver_manager.chrome import ChromeDriverManager
+import bs4_code
+import pyautogui as pg
+from config import contact, archive, chat, message_class, photo_class
 
-driver = webdriver.Chrome("chromedriver.exe")
+def select(xpath, format, text='NULL',click=0):
+    selected = driver.find_element(By.XPATH, xpath.format(format))
+    if click==1:
+        selected.click()
+    if text != 'NULL':
+        print(text)
+    time.sleep(2)
 
 options = webdriver.ChromeOptions()
+options.add_argument(r'--user-data-dir=C:\Users\OFFICE\AppData\Local\Google\Chrome\User Data\Default')
+options.add_argument('--profile-directory=Default')
 
-preference = {"download.default_diretory": r"D:\temp\p"}
+driver = webdriver.Chrome(executable_path=r'C:\Users\OFFICE\PycharmProjects\whatsapp-project\chromedriver.exe',
+                          options=options)
+driver.get("https://web.whatsapp.com")
 
-options.add_experimental_option("prefs", preference)
+i=15
+while(i>0):
+    print("Waiting...{}".format(i))
+    time.sleep(1)
+    i-=1
+print("Logged In")
+time.sleep(2)
+# inp_xpath_search = "//input[@title='Search or start new chat']"
+# input_box_search = WebDriverWait(driver,50).until(lambda driver: driver.find_element(inp_xpath_search))
+# input_box_search.click()
+# time.sleep(2)
+# input_box_search.send_keys(contact)
 
-driver = webdriver.Chrome(chrome_options=options)
+select('//button[@class="{}"]',archive, "archive opened",click=1)
+select('//span[@title="{}"]', contact, "contact opened",1)
+# select('//div[@class="{}"]', chat, "chat selected",1)
+select('//span[@class="{}"]', message_class)
+# driver.execute_script("var scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;")
+pg.click(913,617)
+for i in range(10):
+    pg.press('home')
+    print("home pressed")
+    time.sleep(1)
 
-driver.get("https://web.whatsapp.com/")
-
-time.sleep(25)
-
-"""
-if number in config.py
-    driver(xpath)...
-    
-
-"""
+bs4_code.select_all_text(driver.page_source, message_class)
+bs4_code.select_photo_scr(driver.page_source, photo_class)
+time.sleep(20)
+driver.quit()
