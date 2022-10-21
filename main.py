83254a26b37afe1197b2ec5c9eefdf7f35d834c0
Olzhas <vip0amirkulov@gmail.com>
2022-10-21 14:34:42 +0600

    85-87 надо сделать так что-бы проверял div msg-container на наличие img, и выбрать _3BK98

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 
 import pyautogui as pg
+from lxml import etree, html
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from fucions import taking_sorted_messages, print_time, select, write_to_file
@@ -10,9 +11,7 @@ from config import contact, archive, select_ico, argument1, argument2,\
 
 
 tic = time.time()
-options = webdriver.ChromeOptions()
-options.add_argument(argument1)
-options.add_argument(argument2)
+options = webdriver.ChromeOptions(); options.add_argument(argument1); options.add_argument(argument2)
 
 driver = webdriver.Chrome\
     (executable_path=r'C:\Users\OFFICE\PycharmProjects\whatsapp-project\chromedriver.exe',
@@ -33,14 +32,14 @@ select(driver, By, '//span[@title="{}"]', contact, "contact opened", 1)
 
 pg.click(913, 617)
 i = 0
-a, b = 0, 1
+message_div_sum, message_div_sum2 = 0, 1
 while True:
     pg.press('home')
     print("{}...".format(i), end='')
     time.sleep(0.3)
     if i % 5 == 0:
-        b, a = a, req_url(driver.page_source)
-        if a == b:
+        message_div_sum2, message_div_sum = message_div_sum, req_url(driver.page_source)
+        if message_div_sum == message_div_sum2:
             break
     i += 1
 tic = print_time(tic, time.time(), "step2")
@@ -58,78 +57,57 @@ print('len sorted_m: ', len(sorted_messages), ' - ', sum)
 tic = print_time(tic, time.time(), "array1")
 
 r = open('text.txt', 'r', encoding='utf8')
-n=''
+j_text= ''
 sum = 0
 
 for i in r:
     for j in range(len(sorted_messages)):
+        i_text = str(''.join(i.splitlines()))
         if(sorted_messages[j])!='none':
             temp_line = sorted_messages[j].text.splitlines()
-            # n1 = n
-            n = ''.join(temp_line)
-            k = sorted_messages[j].get_attribute('innerHTML')
-            # if 'img' in sorted_messages[j].get_attribute('innerHTML'):
-            #     if str(''.join(i.splitlines())) == 'Фото 17 Мкр Мухаббат07:59':
-            #         print('{}-'.format(sum),str(''.join(n)))
-            #         sum+=1
-
+            j_text = str(''.join(''.join(temp_line)))
+            j_html = str(sorted_messages[j].get_attribute('innerHTML'))
             # print(sorted_messages[j].get_attribute('innerHTML'))
             # print(str(''.join(i.splitlines())), ' - ', str(''.join(n)))
-            if (str(''.join(n)) in str(''.join(i.splitlines()))):
-                if 'img' in k:
-                    if '_3BK98' in k:
+            if (j_text in i_text):
+                if 'img' in j_html:
+                    if '_3BK98' in j_html:
                         # print(str(sorted_messages[j].get_attribute('innerHTML')))
-                        if "_1-lf9 _3mSPV" not in k:
-                            try:
-                                # print(i.splitlines()[0], ' - ', n)
-                                # print('+', sorted_messages[j].get_attribute('innerHTML'))
-                                # selected worked with select_ico
-                                selected = sorted_messages[j].find_element(By.XPATH, '//div[@class="{}"]'.
-                                                                           format(select_ico))
-                                # selected.click()
-                                driver.execute_script("arguments[0].click();", selected)
-                                time.sleep(1)
-                                print(k)
-                                sorted_messages[j] = 'none'#taking_sorted_messages(driver, By, j, True)
-                                sum+=1
-                                break
-                            except Exception as e:
-                                print("-----------------------\nwrong: {}".format(e))
-                                print(sorted_messages[j])
-                                print(k,'\n--------------------')
-                            finally:
-                                print(str(''.join(n)), ' - ',  str(''.join(i.splitlines())))
+                        if "_1-lf9 _3mSPV" not in j_html:
+                            if '_22Msk' not in j_html:
+                                # sorted_messages[j].find_element(By.XPATH, '//')
+                                try:
+                                    # print(i.splitlines()[0], ' - ', n)
+                                    print('+', j_html)
+                                    # selected worked with select_ico
+                                    document_root = html.document_fromstring(j_html)
+                                    print(etree.tostring(document_root, encoding='unicode', pretty_print=True))
+                                    selected = sorted_messages[j].find_element(By.XPATH, '//div[@data-testid="{}"]'.
+                                                                               format('msg-container'))
+                                    # selected = sorted_messages[j].find_element(By.XPATH, '//div[@class="{}"]'.
+                                    #                                            format('_22Msk'))
+                                    selected1 = sorted_messages[j].find_element(By.XPATH, '//div[@class="{}"]'.
+                                                                               format('_3BK98'))
+                                    # selected.click()
+                                    print('selected: ',selected.get_attribute('innerHTML'))
+                                    if 'image' in selected.get_attribute('innerHTML'):
+                                        print("***")
+                                        driver.execute_script("arguments[0].click();", selected1)
+                                    time.sleep(0.3)
+                                    print(j_html)
+                                    sorted_messages[j]='none'
+                                    # sorted_messages.remove(j)#taking_sorted_messages(driver, By, j, True)
+                                    sum+=1
+                                    break
+                                except Exception as e:
+                                    print("-----------------------\nwrong: {}".format(e))
+                                    print(j_html)
+                                finally:
+                                    print(j_text, ' - ', i_text, '\n--------------------')
                         else:
                             print("---")
 
-
 print("sum = {}".format(sum))
-        # temp_line = j.text.splitlines()
-        # n = ''.join(temp_line)
-        #
-        # # if l < 3:
-        # #     print("i: ", str(i.splitlines()[0]) , "\nn: ", n)
-        # #     print("1: ", j.get_attribute('innerHTML'))
-        # #     print("2: ", j.get_attribute('class'))
-        # # print("2: ", j.find_element(By.XPATH, '//div[@class="{}"]'.format(select_ico)))
-        #
-        # # problem here. need to choose next element if this was selected
-        # if str(n) == str(i.splitlines()[0]):
-        #     l += 1
-        #     print(l)
-        #     print(j.get_attribute('class'))
-        #     if "_3Zpy8" not in str(j.get_attribute('class')):
-        #
-        #         selected = j.find_element(By.XPATH, '//div[@class="{}"]'.format(select_ico))
-        #         selected.click()
-        #
-        #         sorted_messages.remove(j)
-        #
-        #         print("after remove: ", len(sorted_messages))
-        #         print("+: ", j.get_attribute('innerHTML'))
-        #         # pg.press('down')
-        #         break
-#         # select(driver, By, '//span[@class="{}"]', message_class, ms=True)
 
 tic = print_time(tic, time.time(), "array2")
 
