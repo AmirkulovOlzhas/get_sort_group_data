dd802edc0a058263b0ef82bd38261159cd070d33
Olzhas <vip0amirkulov@gmail.com>
2022-11-08 17:57:48 +0600

    3 проблемы *!pg down не работает подключено корректное+ сохранение имен фото парка переименовывание фото def main бесконечная выгрузка
    нужно
    слишком долгое ролит в топ
    подключить ооп
    граф интерЕЙС
    переименовывание енб Абай
    try catch в main
    сохранение

--- stuf/delete_parentheses.py
+++ stuf/delete_parentheses.py
@@ -6,12 +6,11 @@ from os.path import isfile, join
 def rename_file(this_line):
     temp_line_name = this_line.replace(' (', '-')
     temp_line_name = temp_line_name.replace(')', '')
-    os.rename(mypath + '\\' + this_line, mypath + '\\' + temp_line_name)
+    os.rename(mypath + '\\' + this_line, mypath + '\\' + '00' + temp_line_name[29:])
 
 
-mypath = r"C:\Users\OFFICE\Desktop\8112022-"
+mypath = r"C:\Users\OFFICE\Desktop\test"
 all_files_name = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
 
 for line in all_files_name:
     if ('(' in line) & (')' in line):
