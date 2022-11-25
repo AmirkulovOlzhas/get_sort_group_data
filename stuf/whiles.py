86d37d9086c565c9bba8f76b813a2c8a05eda903
Olzhas <vip0amirkulov@gmail.com>
2022-11-25 17:45:29 +0600

    12 проблемы !!!переименования сперва убрать скобки только потом !!!!видео не понимает так как mp4 спускаются вниз { if folder[len(folder)-1].last_simbols(3) != 'mp4'?:    ok else:    rename } sorted_messages_list.txt сохранить сразу в папку(done) удалять архив после использования выявить ошибку если не все фото загрузилиь
    нужно
    слишком долгое ролит в топ
    подключить ооп
    граф интерЕЙС
    тесты

--- stuf/whiles.py
+++ stuf/whiles.py
@@ -1,10 +1,17 @@
-import time
-from fuctions import select
+from os import listdir
+from os.path import isfile, join
 
-# def waiting_wa():
-#     while True:  # waiting for wa
-#         try:
-#             select(driver, By, '//button[@class="{}"]', archive, "archive opened", clicked=1)
-#             break
-#         except:
-#             time.sleep(1)
+from rename_files_names import rename_all_files
+
+mypath = r"D:\WA_photo\1"
+
+all_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+print("len", len(all_files))
+
+rename_all_files(all_files, outside=True)
+# taking updated photo names from folder
+changed_files_name = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+i, temp_value = 0, 0
+for j in range(len(changed_files_name)):
+    print(all_files[j], '  |  ', changed_files_name[j])
+print('---------------- --------------- ----------------- -----------------')
